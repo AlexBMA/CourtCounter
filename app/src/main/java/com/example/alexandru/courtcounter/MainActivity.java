1c964adb8e4350fde75f38e3b6614f8388f45bd0
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-01-17 13:42:01 +0200

    Update 6

--- app/src/main/java/com/example/alexandru/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/alexandru/courtcounter/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int scoreTeamA = 0 ;
+    int scoreTeamA = 0;
     int scoreTeamB = 0;
 
     @Override
@@ -17,11 +17,11 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA();
         displayForTeamB();
     }
+
     /**
      * Displays the given score for Team A.
      */
-    public void displayForTeamA()
-    {
+    public void displayForTeamA() {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(scoreTeamA));
     }
@@ -33,58 +33,59 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(scoreTeamB));
     }
-    /**
-        Adds 3 points for team A
 
+    /**
+     * Adds 3 points for team A
      */
-    public void addTreePointsForTeamA(View view)
-    {
-        scoreTeamA +=3;
+    public void addTreePointsForTeamA(View view) {
+        scoreTeamA += 3;
         displayForTeamA();
     }
 
     /**
-        Add 2 points for team B
-
+     * Add 2 points for team B
      */
-    public void addTwoPointsForTeamA(View view)
-    {
-        scoreTeamA +=2;
+    public void addTwoPointsForTeamA(View view) {
+        scoreTeamA += 2;
         displayForTeamA();
     }
-    /**
-     Add 1 point for team B
 
+    /**
+     * Add 1 point for team B
      */
-    public void addFreeThrowForTeamA(View view)
-    {
-        scoreTeamA +=1;
+    public void addFreeThrowForTeamA(View view) {
+        scoreTeamA += 1;
         displayForTeamA();
     }
+
     /**
-     Adds 3 points for team B
+     * Adds 3 points for team B
      */
-    public void addTreePointsForTeamB(View view)
-    {
-        scoreTeamB +=3;
+    public void addTreePointsForTeamB(View view) {
+        scoreTeamB += 3;
         displayForTeamB();
     }
-    /**
-     Add 2 points for team B
 
+    /**
+     * Add 2 points for team B
      */
-    public void addTwoPointsForTeamB(View view)
-    {
-        scoreTeamB +=2;
+    public void addTwoPointsForTeamB(View view) {
+        scoreTeamB += 2;
         displayForTeamB();
     }
-    /**
-     Add 1 point for team B
 
+    /**
+     * Add 1 point for team B
      */
-    public void addFreeThrowForTeamB(View view)
-    {
-        scoreTeamB +=1;
+    public void addFreeThrowForTeamB(View view) {
+        scoreTeamB += 1;
+        displayForTeamB();
+    }
+
+    public void resetScore(View view) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA();
         displayForTeamB();
     }
 }
